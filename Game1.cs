e87e4394f3e3e950d1e939640da9a8e7ed6080f9
Henrique <henrique.dsc@gmail.com>
2024-03-18 01:17:23 -0300

    Added input class.

--- Game1.cs
+++ Game1.cs
@@ -13,6 +13,7 @@ public class Game1 : Game
     private Vector2 _position;
     private double _speed;
     private const double ACCELERATION = 10;
+    private bool _isBombDropped;
 
     public Game1()
     {
@@ -30,6 +31,7 @@ public class Game1 : Game
 
         _position = new Vector2(_graphics.PreferredBackBufferWidth / 2.0f, 0.0f);
         _speed = 0.0;
+        _isBombDropped = false;
     }
 
     protected override void LoadContent()
@@ -44,23 +46,20 @@ public class Game1 : Game
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
 
-        /*
-        // S = S0 + V0t + (at2/2)
-        _position.Y = _position.Y + 
-        (float)(_speed * gameTime.ElapsedGameTime.TotalSeconds) + 
-        (float)(ACCELERATION * gameTime.ElapsedGameTime.TotalSeconds * gameTime.ElapsedGameTime.TotalSeconds);
-
-        // V = V0 + at
-        _speed = _speed + (ACCELERATION * gameTime.ElapsedGameTime.TotalSeconds);
-        */
-
-        _speed = _speed + (ACCELERATION * gameTime.ElapsedGameTime.TotalSeconds);
-        _position.Y = _position.Y + (float)_speed;
-
-        if (_position.Y > _graphics.PreferredBackBufferHeight)
+        if (Input.GetKeyDown(Keys.Space))
         {
             _position.Y = 0.0f;
+            _speed = 0.0f;
+            _isBombDropped = true;
         }
+
+        if (_isBombDropped)
+        {
+            _speed = _speed + (ACCELERATION * gameTime.ElapsedGameTime.TotalSeconds);
+            _position.Y = _position.Y + (float)_speed;
+        }
+
+        Input.Update();
 
         base.Update(gameTime);
     }
