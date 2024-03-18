7f162154c32bc6096404eafe3af5f8532b9ae9a6
Henrique <henrique.dsc@gmail.com>
2024-03-17 22:00:56 -0300

    Added MU code.

--- Game1.cs
+++ Game1.cs
@@ -8,6 +8,10 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    
+    private Texture2D _arrow;
+    private Vector2 _position;
+    const double SPEED = 400.0f;
 
     public Game1()
     {
@@ -18,21 +22,24 @@ public class Game1 : Game
 
     protected override void Initialize()
     {
-        // TODO: Add your initialization logic here
-
         base.Initialize();
+
+        _position = new Vector2(0, (_graphics.PreferredBackBufferHeight / 2.0f));
     }
 
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+        _arrow = Content.Load<Texture2D>("arrow");
     }
 
     protected override void Update(GameTime gameTime)
     {
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
+ 
+        _position.X = _position.X + (float)(SPEED * gameTime.ElapsedGameTime.TotalSeconds);
 
         base.Update(gameTime);
     }
@@ -42,7 +49,7 @@ public class Game1 : Game
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
         _spriteBatch.Begin();
-        
+        _spriteBatch.Draw(_arrow, _position, Color.White);
         _spriteBatch.End();
 
         base.Draw(gameTime);
