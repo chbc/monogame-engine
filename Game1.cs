1c277aea0fa84cdc20e8ef6f79abd5c3fc708e20
Henrique <henrique.dsc@gmail.com>
2024-03-23 23:00:59 -0300

    Removed old code.

--- Game1.cs
+++ Game1.cs
@@ -9,12 +9,6 @@ public class Game1 : Game
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
-    private Texture2D _bomb;
-    private Vector2 _position;
-    private double _speed;
-    private const double ACCELERATION = 10;
-    private bool _isBombDropped;
-
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
@@ -25,41 +19,17 @@ public class Game1 : Game
     protected override void Initialize()
     {
         base.Initialize();
-
-        _graphics.PreferredBackBufferHeight = 800;
-        _graphics.ApplyChanges();
-
-        _position = new Vector2(_graphics.PreferredBackBufferWidth / 2.0f, 0.0f);
-        _speed = 0.0;
-        _isBombDropped = false;
     }
 
     protected override void LoadContent()
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-        _bomb = Content.Load<Texture2D>("bomb");
     }
 
     protected override void Update(GameTime gameTime)
     {
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
-
-        if (Input.GetKeyDown(Keys.Space))
-        {
-            _position.Y = 0.0f;
-            _speed = 0.0f;
-            _isBombDropped = true;
-        }
-
-        if (_isBombDropped)
-        {
-            _speed = _speed + (ACCELERATION * gameTime.ElapsedGameTime.TotalSeconds);
-            _position.Y = _position.Y + (float)_speed;
-        }
-
-        Input.Update();
 
         base.Update(gameTime);
     }
@@ -69,7 +39,6 @@ public class Game1 : Game
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
         _spriteBatch.Begin();
-        _spriteBatch.Draw(_bomb, _position, Color.White);
         _spriteBatch.End();
 
         base.Draw(gameTime);
